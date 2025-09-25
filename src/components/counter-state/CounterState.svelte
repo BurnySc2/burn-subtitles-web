e097b1999e81bb1e60d8af29fccf14e2c9c1022a
BurnySc2 <gamingburny@gmail.com>
2025-09-25 15:15:43 +0200

    Initial commit

---
+++ src/components/counter-state/CounterState.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { use_local_storage_state } from '$lib/persistent-storage.svelte'
+
+	let my_counter_state = use_local_storage_state('my_counter_state', 0)
+</script>
+
+<button
+	class="rounded border-2 border-black p-2"
+	onclick={() => {
+		my_counter_state.value += 1
+	}}>Increment</button
+>
+<div>
+	{my_counter_state.value}
+</div>
