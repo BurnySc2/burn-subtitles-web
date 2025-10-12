3a5c3d26374351eaaacda35c83ff655659a42c26
BurnySc2 <gamingburny@gmail.com>
2025-10-12 12:59:27 +0000

    Fix page not being cut off at the top

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,6 @@
 import ASSSubtitleProcessor from "$lib/components/ASSSubtitleProcessor.svelte"
 </script>
 
-<main class="container mx-auto px-4 py-8">
-	<div class="flex h-screen flex-col items-center justify-center">
+<div class="flex flex-col items-center justify-center py-8">
 	<ASSSubtitleProcessor />
 	</div>
-</main>
