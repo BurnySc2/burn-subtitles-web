20a3b14459a485ea71285ae4da90a9ac4211aa77
BurnySc2 <gamingburny@gmail.com>
2025-10-12 12:58:57 +0000

    Make site use default .ass subtitle burner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-import FfmpegDemo from "$lib/components/FfmpegSRTDemo.svelte"
+import ASSSubtitleProcessor from "$lib/components/ASSSubtitleProcessor.svelte"
 </script>
 
 <main class="container mx-auto px-4 py-8">
 	<div class="flex h-screen flex-col items-center justify-center">
-		<FfmpegDemo />
+	<ASSSubtitleProcessor />
 	</div>
 </main>
