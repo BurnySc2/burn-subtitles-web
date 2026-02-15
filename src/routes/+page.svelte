388ce06bd89eff68cdbd7ab2fcf16b7e9f9a059e
burnysc2 <gamingburny@gmail.com>
2026-02-15 23:06:41 +0100

    Add postmessage for iframe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,28 @@
 <script lang="ts">
+import { onMount } from "svelte"
 import ASSSubtitleProcessor from "$lib/components/ASSSubtitleProcessor.svelte"
+
+onMount(() => {
+	function sendHeight() {
+		const height = document.documentElement.scrollHeight
+		window.parent.postMessage(
+			{
+				type: "resize-iframe",
+				height: height,
+			},
+		)
+	}
+
+	// Send once after load
+	sendHeight()
+
+	// Observe DOM changes (better than resize listener)
+	const observer = new ResizeObserver(() => {
+		sendHeight()
+	})
+
+	observer.observe(document.body)
+})
 </script>
 
 <div class="flex flex-col items-center justify-center py-8">
