d3689f74c9bdb2f979c2fe5c57cf3abf3fae9e6d
burnysc2 <gamingburny@gmail.com>
2026-03-17 00:22:39 +0100

    Delete srt subtitler

--- src/routes/srt/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-import SubtitleProcessor from "$lib/components/SubtitleProcessor.svelte"
-</script>
-
-<div class="flex flex-col items-center justify-center py-8"><SubtitleProcessor /></div>
